89ca1fe5760b4e063c8634aebe393fa062363a1e
Mason Hirst <mason.j.hirst@gmail.com>
2022-09-30 10:26:37 -0600

    First save of the test file. sdlc.js has been completed

---
+++ sdlc.js
@@ -0,0 +1,32 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = "The second step, in which the requirements of the project are determined. Developers will discuss what technologies will be needed (determine the stack needed), how many people will need to be involved, and in a business setting, the cost and feasability of the project."
+
+const design = "The third step, where designers will plan the project according to the specifications set in the first two steps. Essentially they are making a blueprint for the project, like a blueprint for a house. The blueprint is split into task sections, which each profession is responsible for building."
+
+const implementation = "During the fourth step, the actual work of writing code begins. Devs are giving assignments for parts of the overall project, and are overseen by project managers. Devs collaborate to make sure the code works, and fits in with the entire project."
+
+const testingAndIntegration = "The fifth step, where the code is tested many times, to find bugs and inefficiencies. Every aspect and possible use of the code is tested to make sure it works as designed, and is the most efficient/clean way possible. Good code is officially added to the overall project, until the project is determined to be complete."
+
+const maintain = "The 6th and final phase, where the software is monitored for statistics, performance, and especially bugs/errors. Reported bugs are fixed ASAP, to ensure the software users have a positive experience. The software is also updated as user need dictates."
